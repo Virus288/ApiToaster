6a451265c717c8e17bd7ad88c7280ebdf5ebbd10
DominikDO98 <dominiklas13@wp.pl>
2024-11-13 13:38:44 +0100

    Refactor: Moved decode test to separate test suite

--- __tests__/unit/decode/index.test.ts
+++ __tests__/unit/decode/index.test.ts
@@ -1,14 +1,14 @@
-import { afterAll, afterEach, beforeAll, beforeEach, describe, expect, it, jest } from '@jest/globals';
+import { afterAll, afterEach, beforeAll, beforeEach, describe, jest, it, expect } from '@jest/globals';
 import express from 'express';
 import fs from 'fs';
-import FileWriter from '../../../src/module/files/writer.js';
-import TimeTravel from '../../../src/module/timeTravel/index.js';
+import Decoder from '../../../src/module/decode/index.js';
 import defaultConfig from '../../../src/tools/config.js';
 import State from '../../../src/tools/state.js';
 import { IFullError } from '../../../types/error.js';
+import { INotFormattedLogEntry } from '../../../types/logs.js';
+import FileWriter from '../../../src/module/files/writer.js';
 
-describe('Time Travel', () => {
-  let fetchMock: unknown;
+describe('Decoder', () => {
   const clear = async (target?: string): Promise<void> => {
     return new Promise<void>((resolve) => {
       fs.rmdir(target ?? 'Toaster', { recursive: true }, (_err) => {
@@ -16,9 +16,8 @@ describe('Time Travel', () => {
       });
     });
   };
-
-  const timeTravel = new TimeTravel();
   const fileWriter = new FileWriter();
+  const decoder = new Decoder();
   const defaultReq: Partial<express.Request> = {
     method: 'POST',
     headers: {
@@ -30,16 +29,8 @@ describe('Time Travel', () => {
     },
     body: {},
   };
-
   beforeAll(() => {
     State.config = { ...defaultConfig(), ip: true };
-    fetchMock = jest.spyOn(global, 'fetch').mockImplementation(() =>
-      Promise.resolve({
-        ok: true,
-        status: 200,
-        json: async () => {},
-      } as Response),
-    );
   });
 
   beforeEach(async () => {
@@ -55,45 +46,43 @@ describe('Time Travel', () => {
   afterAll(() => {
     jest.restoreAllMocks();
   });
-
-  // describe('Should throw', () => {
-  //describe('No data passed', () => {});
-  //describe('Incorrect data', () => {});
-  // });
-
   describe('Should pass', () => {
-    it(`init - sends requests`, async () => {
+    it(`decode - return request`, async () => {
       let error: IFullError | undefined = undefined;
-      let callback: unknown | undefined;
+      let callback: [string, INotFormattedLogEntry][] = [];
       try {
         await fileWriter.init(defaultReq as express.Request);
-        callback = await timeTravel.init({ port: 0 });
+        callback = await decoder.init();
       } catch (err) {
         error = err as IFullError;
       }
       expect(error).toBeUndefined();
-      expect(callback).toBeUndefined();
-      expect(fetchMock).toHaveBeenCalledWith(`http://localhost:0`, {
-        method: 'POST',
-        headers: {
-          'X-Toaster': 'true',
-          header: 'val',
+      expect(callback.length).toEqual(1);
+      expect(callback[0]).toEqual([
+        expect.anything(),
+        {
+          method: 'POST',
+          headers: {
+            header: 'val',
+          },
+          occured: expect.anything(),
+          queryParams: {
+            key: 'value',
+          },
+          body: {},
         },
-        body: '{}',
-      });
-      expect(fetchMock).toHaveBeenCalledTimes(1);
+      ]);
     });
-    it(`init - no request to be send`, async () => {
+    it(`decode - no request to return`, async () => {
       let error: IFullError | undefined = undefined;
-      let callback: unknown | undefined;
+      let callback: [string, INotFormattedLogEntry][] = [];
       try {
-        callback = await timeTravel.init({ port: 0 });
+        callback = await decoder.init();
       } catch (err) {
         error = err as IFullError;
       }
       expect(error).toBeUndefined();
-      expect(callback).toBeUndefined();
-      expect(fetchMock).toHaveBeenCalledTimes(0);
+      expect(callback.length).toEqual(0);
     });
   });
 });
