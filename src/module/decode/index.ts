a5556e8a16d475793e7603294c3ff668b7564a91
DominikDO98 <dominiklas13@wp.pl>
2024-11-13 13:00:10 +0100

    Refactor: Created Decoder class and moved decode and savedecoded methods to it

---
+++ src/module/decode/index.ts
@@ -0,0 +1,54 @@
+import Log from 'tools/logger.js';
+import FileReader from '../files/reader.js';
+import FileWriter from '../files/writer.js';
+import type { ILogs, ILogsProto, INotFormattedLogEntry } from '../../../types';
+
+export default class Decoder {
+  private _fileWriter: FileWriter;
+  private _fileReader: FileReader;
+
+  constructor() {
+    this._fileWriter = new FileWriter();
+    this._fileReader = new FileReader();
+  }
+
+  private get fileWriter(): FileWriter {
+    return this._fileWriter;
+  }
+
+  private get fileReader(): FileReader {
+    return this._fileReader;
+  }
+
+  /**
+   * Decode file.
+   * @description Decode targeted file.
+   * @param fileName Target file.
+   * @returns {void} Void.
+   * @async
+   */
+  async init(fileName?: string): Promise<[string, INotFormattedLogEntry][]> {
+    Log.debug('Time travel', 'Decoding');
+
+    const logs = this.readLogs(fileName);
+
+    return this.fileReader.prepareLogs(logs.logs);
+  }
+
+  private readLogs(fileName?: string): ILogsProto | ILogs {
+    return this.fileReader.init(fileName);
+  }
+  /**
+   * Decode file.
+   * @description Decode targeted file.
+   * @param fileName Target file.
+   * @returns {void} Void.
+   * @async
+   */
+  async saveDecoded(fileName?: string): Promise<void> {
+    Log.debug('Time travel', 'Saving');
+
+    const logs = await this.init(fileName);
+    this.fileWriter.save(`decoded_${fileName}`, logs);
+  }
+}
